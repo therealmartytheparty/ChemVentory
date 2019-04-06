a9424c49dd44b509a082a9aaf9219e4228defd97
ShraibergK <kaishraiberg1998@hotmail.com>
2019-04-06 17:27:49 -0400

    file I/O
    finished file I/O so we can read in all the chemical object files

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from list_class import *
 from chemical_class import *
 from GUI.finalgu import *
+from listwidgetops import *
 import sys
 from PyQt5 import QtGui, uic
 
@@ -11,33 +12,10 @@ if __name__ == "__main__":
     ui = Ui_MainWindow()
     ui.setupUi(MainWin)
     MainWin.show()
-    #additems
 
+    list = listloader()
 
-    chem_1 = Chemical('Chemical 1', 'CF1', 'Company1', 600, 500, 100)
-    chem_2 = Chemical('Chemical 2', 'CF2', 'Company2', 500, 400, 200)
-    chem_3 = Chemical('Chemical 3', 'CF3', 'Company3', 400, 300, 300)
-    chem_4 = Chemical('Chemical 4', 'CF4', 'Company4', 300, 200, 400)
-    chem_5 = Chemical('Chemical 5', 'CF5', 'Company5', 200, 100, 500)
-    chem_6 = Chemical('Chemical 6', 'CF6', 'Company6', 100, 50, 600)
-
-    list = list()
-    list.add(chem_1)
-    list.add(chem_2)
-    list.add(chem_3)
-    list.add(chem_4)
-    list.add(chem_5)
-    list.add(chem_6)
-
-    list.print()
-
-    chem_1.file_out()
-    chem_2.file_out()
-    chem_3.file_out()
-    chem_4.file_out()
-    chem_5.file_out()
-    chem_6.file_out()
-
-    chem_1.file_in()
+    print(list[0].name)
+    print(list[0].barC)
 
     sys.exit(app.exec_())
--- chemical_class.py
+++ chemical_class.py
@@ -1,65 +1,36 @@
-import json
-
 class Chemical:
 
     num_of_Chems = 0
 
-    def __init__(self, name, formula, supplier, massI, massUpdate, age):
-        self.barC = Chemical.num_of_Chems + 1
-        self.name = name
-        self.formula = formula
-        self.supplier = supplier
-        self.massI = massI
-        self.massUpdate = massUpdate
-        self.age = age
+    def __init__(self, name=None, formula=None, supplier=None, massI=None, massUpdate=None, age=None):
+        if name != None:
+            self.barC = Chemical.num_of_Chems + 1
+            self.formula = formula
+            self.name = name
+            self.supplier = supplier
+            self.massI = massI
+            self.massUpdate = massUpdate
+            self.age = age
 
         Chemical.num_of_Chems += 1
 
-    def file_out(self):
-        temp = str(self.barC) + '.txt'
-        data = {}
-        data['chem'] = []
-        data['chem'].append({
-            'barC': self.barC,
-            'name': self.name,
-            'formula': self.formula,
-            'supplier': self.supplier,
-            'massI': self.massI,
-            'massUpdate': self.massUpdate,
-            'age': self.age
-        })
-        with open("Chemicals/" + temp, 'w') as outfile:
-            strrr = str(self.barC) +"\n"
-            print(strrr)
-            outfile2 = open("Chemicals/Chembarc.txt",'a')
-            outfile2.write(strrr)
-            json.dump(data, outfile)
-        outfile.close()
-        outfile2.close()
+    def setbarC(self,barC):
+        self.barC = barC
 
+    def setname(self,name):
+        self.name = name
 
-    def file_in(self):
-        barcode = []
-        with open("Chemicals/Chembarc.txt",'r') as file:
-            line = file.readline()
-            cnt = 1
-            while line:
-                print("Line {}: {}".format(cnt,line.strip()))
-                line = file.readline()
-                cnt += 1
-        file.close()
+    def setformula(self,formula):
+        self.formula = formula
 
-'''
-        temp_in = str(self.barC) + '.txt'
-        with open("Chemicals/" + temp_in) as json_file:
-            data = json.load(json_file)
-            for p in data['chem']:
-                self.barC = p['barC']
-                self.name = p['name']
-                self.formula = p['formula']
-                self.supplier = p['supplier']
-                self.massI = p['massI']
-                self.massUpdate = p['massUpdate']
-                self.age = p['age']
-        json_file.close()
-'''
+    def setsupplier(self,supplier):
+        self.supplier = supplier
+
+    def setmassI(self,massI):
+        self.massI = massI
+
+    def setmassUpdate(self,massUpdate):
+        self.massUpdate = massUpdate
+
+    def setage(self,age):
+        self.age = age
